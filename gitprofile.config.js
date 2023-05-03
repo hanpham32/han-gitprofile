d925ff4c64edbdf2b021a430ab445191faadf442
Han Mac <phamhan@uw.edu>
2023-05-02 19:49:44 -0700

    update projects exclusion

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -7,7 +7,7 @@ const config = {
     limit: 6, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: ['cse163-final', 'website', 'old-personal-website'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['hanpham32', 'han-gitprofile'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
